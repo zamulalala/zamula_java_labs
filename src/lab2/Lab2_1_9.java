b88837948d27480f0c9e58f819e9946e9474ed07
zamulalala <ritazamula2001@gmail.com>
2023-03-05 17:38:50 +0300

    add lab2_1_9

---
+++ src/lab2/Lab2_1_9.java
@@ -0,0 +1,43 @@
+//9. Используя оператор switch, написать программу, которая выводит на экран сообщения
+// о принадлежности некоторого значения k интервалам (-10k, 0], (0, 5], (5, 10], (10, 10k].
+
+package lab2;
+
+import java.util.Scanner;
+
+import static lab2.lab2_1_8.printInfo;
+
+public class Lab2_1_9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input k: ");
+        int k = in.nextInt();
+        choiceInterval(k);
+        printInfo();
+    }
+
+    public static void choiceInterval(int k) {
+        String result = switch (getInterval(k)) {
+            case 1 -> "k is in range (-10k, 0]";
+            case 2 -> "k is in range (0, 5]";
+            case 3 -> "k is in range (5, 10]";
+            case 4 -> "k is in range (10, 10k]";
+            default -> "error";
+        };
+        System.out.println(result);
+    }
+
+    public static int getInterval(int k) {
+        int interval;
+        if (k <= 0) {
+            interval = 1;
+        } else if (k <= 5) {
+            interval = 2;
+        } else if (k <= 10) {
+            interval = 3;
+        } else {
+            interval = 4;
+        }
+        return interval;
+    }
+}
